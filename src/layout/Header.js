7dd9880a6aa577308dde6593949dbeedbf691f03
Kenny <kenji.maddela01@gmail.com>
2022-09-22 01:51:04 -0400

    Set min width to the header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,7 +6,7 @@ import HamburgerButton from './header/HamburgerButton'
 import MobileMenu from './header/MobileMenu'
 
 const containerClasses =
-	'container max-w-screen-2xl mx-auto px-6 py-18 pt-3 bg-slate-100'
+	'container min-w-full mx-auto px-6 py-18 pt-3 bg-slate-100'
 const navBarClasses =
 	'flex items-center justify-between font-bold text-slate-800'
 
