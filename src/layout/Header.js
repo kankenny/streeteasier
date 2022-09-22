5316a36776ce4f89ce7b1422d4c759a9b45e7cf1
Kenny <kenji.maddela01@gmail.com>
2022-09-22 01:04:39 -0400

    Refactor header and mobile menu button

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Logo from '../assets/logo.png'
 
 const Header = () => {
+	const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false)
+
+	const toggleMenuHandler = () => {
+		setMobileMenuIsOpen((prevState) => !prevState)
+	}
+
 	return (
 		<section id="header">
 			{/* Header Container */}
-			<div className="container max-w-screen-2xl mx-auto px-6 py-12 pt-3">
+			<div className="container max-w-screen-2xl mx-auto px-6 py-18 pt-3 bg-slate-100">
 				<nav className="flex items-center justify-between font-bold text-slate-800">
 					{/* Logo */}
 					<img src={Logo} alt="Logo" />
@@ -34,7 +40,10 @@ const Header = () => {
 						<button
 							id="menu-btn"
 							type="button"
-							className="z-40 block hamburger md:hidden focus:outline-none"
+							onClick={toggleMenuHandler}
+							className={`z-40 block hamburger md:hidden focus:outline-none ${
+								mobileMenuIsOpen ? 'open' : ''
+							}`}
 						>
 							<span className="hamburger-top"></span>
 							<span className="hamburger-middle"></span>
@@ -46,21 +55,38 @@ const Header = () => {
 				{/* Mobile Menu */}
 				<div
 					id="menu"
-					className="absolute top-0 bottom-0 left-0 hidden flex-col self-end w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-black uppercase bg-black"
+					className={`absolute top-0 bottom-0 left-0 flex-col self-end w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-slate-100 uppercase bg-slate-800 ${
+						!mobileMenuIsOpen ? 'hidden' : 'flex'
+					}`}
 				>
-					<a href="#body" className="hover:text-pink-500">
+					<a
+						href="#body"
+						className="hover:text-purple-500 duration-200"
+					>
 						About
 					</a>
-					<a href="#body" className="hover:text-pink-500">
+					<a
+						href="#body"
+						className="hover:text-purple-500 duration-200"
+					>
 						Careers
 					</a>
-					<a href="#body" className="hover:text-pink-500">
+					<a
+						href="#body"
+						className="hover:text-purple-500 duration-200"
+					>
 						Events
 					</a>
-					<a href="#body" className="hover:text-pink-500">
+					<a
+						href="#body"
+						className="hover:text-purple-500 duration-200"
+					>
 						Products
 					</a>
-					<a href="#body" className="hover:text-pink-500">
+					<a
+						href="#body"
+						className="hover:text-purple-500 duration-200"
+					>
 						Support
 					</a>
 				</div>
