91203ddf69827b087b451d8203523c19b374b521
Kenny <kenji.maddela01@gmail.com>
2022-09-24 15:38:52 -0400

    Differentiate main link from all other links

--- src/layout/header/DesktopMenu.js
+++ src/layout/header/DesktopMenu.js
@@ -4,7 +4,7 @@ import Link from '../../components/ui/Link'
 
 const DesktopMenu = () => {
 	return (
-		<div className="hidden h-10 font-alata md:flex md:space-x-8">
+		<div className="hidden h-10 font-alata md:flex md:items-center md:space-x-8 min-h-fit">
 			<Link linkText="About" />
 			<Link linkText="Members" />
 			<Link linkText="Contacts" />
