64e57daae4dd95cb9de77f9993366bb8a66b5092
Kenny <kenji.maddela01@gmail.com>
2022-09-24 03:45:03 -0400

    Implement tech stack

--- src/layout/body/home/GetStarted.js
+++ src/layout/body/home/GetStarted.js
@@ -6,7 +6,7 @@ const headerClasses = 'text-4xl text-center uppercase md:text-left md:text-5xl'
 
 function GetStarted() {
 	return (
-		<div className="container mx-auto w-max">
+		<div className="container mx-auto w-max mb-32">
 			<Link
 				className={headerClasses}
 				linkText="Let's Get Started!"
