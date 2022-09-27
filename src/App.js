623639ab6b122345ca486194ff08acdfad1f0310
Danny Chen <dannychen8102@gmail.com>
2022-09-27 19:39:25 -0400

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { 
 	Fragment, 
-	// useState,
-	// useEffect
 } from 'react'
-
-// import {
-// 	collection, 
-// 	getDocs,
-// } from 'firebase/firestore'
 
 import Header from './layout/Header'
 import Body from './layout/Body'
 import Footer from './layout/Footer'
 import './App.css'
-// import { db } from './firebase-config'
 
 function App() {
 	return (
