4a53e5a6d3737b0f7cefe3e91fc1d12ac4fe449e
Kenny <kenji.maddela01@gmail.com>
2022-09-27 01:13:44 -0400

    Split SignUp component into smaller components particularly the input component

--- src/layout/body/user-login-signup/signup/SignUp.js
+++ src/layout/body/user-login-signup/signup/SignUp.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 
-import Card from '../../../../components/ui/Card'
-import img from '../../../../assets/signup-login/signup.jpg'
+import Input from '../../../../components/ui/Input'
 import Overview from '../../../../components/ui/Overview'
 
+import Card from '../../../../components/ui/Card'
+import SolidButton from '../../../../components/ui/button/SolidButton'
+import ExistingUserPrompter from '../../../../components/ui/signup/ExistingUserPrompter'
+
+import img from '../../../../assets/signup-login/signup.jpg'
 import { motion } from 'framer-motion'
+
+const signUpContainerClasses =
+	'w-full md:w-1/2 lg:w-1/3 mx-auto my-12 ml-10 min-h-[15rem]'
 
 const SignUp = () => {
 	const [userInfo, setUserInfo] = useState({
@@ -83,78 +89,57 @@ const SignUp = () => {
 				subtitle="Only a couple more steps to find your home and roommates"
 			/>
 			<Card className="max-w-6xl mx-auto flex flex-col md:flex-row space-x-8">
-				<div className="w-full md:w-1/2 lg:w-1/3 mx-auto my-12 ml-10 min-h-[15rem]">
+				<div className={signUpContainerClasses}>
 					<h1 className="text-2xl font-bold">Register</h1>
 					<form className="flex flex-col mt-4 space-y-5">
-						<input
+						<Input
 							type="text"
 							name="firstName"
-							className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
 							placeholder="First Name"
 							value={userInfo.firstName}
 							onChange={firstNameChangeHandler}
 						/>
-						<input
+						<Input
 							type="text"
 							name="lastName"
-							className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
 							placeholder="Last Name"
 							value={userInfo.lastName}
 							onChange={lastNameChangeHandler}
 						/>
-						<input
+						<Input
 							type="number"
-							name="number"
-							className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+							name="age"
 							placeholder="Age"
 							value={userInfo.age}
 							onChange={ageChangeHandler}
 						/>
-						<input
+						<Input
 							type="email"
 							name="email"
-							className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-							placeholder="Email address"
+							placeholder="Email Address"
 							value={userInfo.emailAddress}
 							onChange={emailChangeHandler}
 						/>
-
-						<input
+						<Input
 							type="password"
 							name="password"
-							className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
 							placeholder="Password"
-							autoComplete="on"
 							value={userInfo.password}
 							onChange={passwordChangeHandler}
 						/>
-						<input
+						<Input
 							type="password"
-							name="repeat-password"
-							className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+							name="confirmPassword"
 							placeholder="Confirm Password"
-							autoComplete="on"
 							value={userInfo.confirmPassword}
 							onChange={confirmPasswordChangeHandler}
 						/>
-						<button
-							type="submit"
-							className="mt-4 px-4 py-3  leading-6 text-base rounded-md border border-transparent focus:outline-none bg-blue-500 text-blue-100 hover:text-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer inline-flex items-center w-full justify-center font-medium min-w-30 max-w-[20rem]"
+						<SolidButton
+							buttonText="Register"
+							buttonType="submit"
 							onClick={onUserSubmitHandler}
-						>
-							Register
-						</button>
-						<div className="flex flex-col items-center">
-							<p className="mt-1 text-xs font-light text-gray-500 ">
-								Are you an existing user?
-								<Link
-									className="ml-1 font-medium text-blue-400"
-									to="/login"
-								>
-									Sign in now
-								</Link>
-							</p>
-						</div>
+						/>
+						<ExistingUserPrompter />
 					</form>
 				</div>
 				<img
