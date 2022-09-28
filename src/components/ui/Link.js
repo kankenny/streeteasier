d4b30d15a33acf7e38174c8abcf40726eadbf55f
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2022-09-28 19:00:53 -0400

    Fix font size of the 'Lets Get Started' button in the body (i.e. not in the header)

--- src/components/ui/Link.js
+++ src/components/ui/Link.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Link = ({ linkText, type, className, urlPath }) => {
-	const activeLinkClasses = `bg-fuchsia-700 rounded-full text-white px-4 py-2 group text-zinc-900 transition-all duration-200 ease-in-out ${className} ${
+	const activeLinkClasses = `text-sm bg-fuchsia-700 rounded-full text-white px-4 py-2 group text-zinc-900 transition-all duration-200 ease-in-out ${className} ${
 		type === 'mainLink'
 			? ' bg-blue-700 align-text-middle rounded-full px-4 py-2 shadow-2xl'
 			: ''
 	}`
 
-	const inactiveLinkClasses = `group text-zinc-900 transition-all duration-200 ease-in-out ${className} ${
+	const inactiveLinkClasses = `text-sm group text-zinc-900 transition-all duration-200 ease-in-out ${className} ${
 		type === 'mainLink'
 			? ' bg-blue-700 align-text-middle rounded-full px-4 py-2 shadow-2xl'
 			: ''
@@ -22,7 +22,7 @@ const Link = ({ linkText, type, className, urlPath }) => {
 			}}
 		>
 			<span
-				className={`text-sm bg-left-bottom bg-gradient-to-r bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out ${
+				className={`bg-left-bottom bg-gradient-to-r bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out ${
 					type === 'mainLink'
 						? 'text-white from-white to-white'
 						: 'from-zinc-900 to-zinc-900'
