f6e2bdd2404ae27503488d54f82d57995fd7efa4
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2022-09-28 18:36:14 -0400

    Adjust text size of the header links which affects the overall height of the header

--- src/layout/header/DesktopMenu.js
+++ src/layout/header/DesktopMenu.js
@@ -4,7 +4,7 @@ import Link from '../../components/ui/Link'
 
 const DesktopMenu = () => {
 	return (
-		<div className="hidden h-10 font-alata md:flex md:items-center md:space-x-8 min-h-fit">
+		<div className="hidden font-alata md:flex md:items-center md:space-x-8 min-h-fit">
 			<Link linkText="Home" urlPath="welcome" />
 			<Link linkText="About" urlPath="about" />
 			<Link linkText="Members" urlPath="members" />
