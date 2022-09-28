d13ad23c3677c556c43e6c937cdcc493e22e1a93
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2022-09-28 18:09:33 -0400

    Fix user-login-signup format by adding a bottom margin on the Overview component

--- src/layout/body/user-login-signup/UserLoginSignup.js
+++ src/layout/body/user-login-signup/UserLoginSignup.js
@@ -13,7 +13,7 @@ function UserLoginSignup() {
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			exit={{ opacity: 0 }}
-			className=""
+			className="mt-[8.5rem]"
 		>
 			<Card className="mx-auto max-w-[60rem] flex flex-col my-auto">
 				<Title
