cef2e88f044cbefbd1cef79e0d7735cef15016b1
Kenny <kenji.maddela01@gmail.com>
2022-09-30 01:41:37 -0400

    Adjust max width of 404 container by wrapping it inside Card component

--- src/layout/body/apartments/Apartments.js
+++ src/layout/body/apartments/Apartments.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Card from '../../../components/ui/Card'
 
 function Apartments() {
-	return <Card className="h-full">Test</Card>
+	return <Card className="h-full mx-20 mb-0">Test</Card>
 }
 
 export default Apartments
