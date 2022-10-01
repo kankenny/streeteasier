384fc6cb11e6757ebc80f11748be68b9b23ebff9
Kenny <kenji.maddela01@gmail.com>
2022-10-01 18:07:39 -0400

    Edit overview text of the roommates component

--- src/components/roommates/Roommates.js
+++ src/components/roommates/Roommates.js
@@ -9,8 +9,8 @@ function Roommates() {
 	return (
 		<div className="min-h-full">
 			<Overview
-				title="Apartments"
-				subtitle="Search for your next home"
+				title="Roommates"
+				subtitle="Match with the right roommate"
 				paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim similique iure sit voluptate excepturi tenetur. Corrupti sed asperiores nesciunt quibusdam."
 			/>
 			<Heading heading="Matched Roommates" />
