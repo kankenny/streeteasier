5d835dc888778ebc9250e76f53f5eb23e87614e4
Danny Chen <dannychen8102@gmail.com>
2022-11-28 15:39:59 -0500

    Implemented address auto-complete + new buggy search box to input address

--- src/components/apartments/ApartmentSearch.js
+++ src/components/apartments/ApartmentSearch.js
@@ -1,40 +1,75 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from "react";
 
-import Heading from '../ui/Heading'
-import ApartmentSearchContainer from './ApartmentSearchContainer'
-import Input from '../ui/Input'
-import ApartmentsList from './ApartmentsList'
-import DUMMY_DATA2 from './DUMMY_APARTMENTS'
-import getApartments from '../../api/getApartments'
+import Heading from "../ui/Heading";
+import ApartmentSearchContainer from "./ApartmentSearchContainer";
+import ApartmentsList from "./ApartmentsList";
+import DUMMY_DATA2 from "./DUMMY_APARTMENTS";
+import getApartments from "../../api/getApartments";
+import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
+import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox";
 
 function ApartmentSearch() {
-	const zipCodeRef = useRef('')
-	const zipCode = zipCodeRef.current.value
-	const [queriedApartments, setQueriedApartments] = useState(DUMMY_DATA2)
+	const zipCodeRef = useRef("");
+	const zipCode = zipCodeRef.current.value;
+	const [queriedApartments, setQueriedApartments] = useState(DUMMY_DATA2);
+	const [selected, setSelected] = useState(null);
 
 	const zipCodeOnSubmitHandler = (e) => {
-		e.preventDefault()
-		getApartments(zipCode, setQueriedApartments)
-		console.log(queriedApartments)
-	}
+		e.preventDefault();
+		getApartments(zipCode, setQueriedApartments);
+		console.log(queriedApartments);
+	};
+
+	const PlacesAutoComplete = ({ setSelected }) => {
+		const {
+			ready,
+			value,
+			setValue,
+			suggestions: { status, data },
+			clearSuggestions,
+		} = usePlacesAutocomplete();
+
+		return (
+			<Combobox style={{ position: "relative"}}>
+				<ComboboxInput
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+					disabled={!ready}
+					className="combobox-input"
+					placeholder="Search an address"
+				/>
+
+				<ComboboxPopover>
+					<ComboboxList>
+						{status === "OK" &&
+							data.map(({ place_id, description }) => (
+								<ComboboxOption key={place_id} value={description} />
+							))}
+					</ComboboxList>
+				</ComboboxPopover>
+			</Combobox>
+		);
+	};
 
 	return (
 		<ApartmentSearchContainer>
 			<Heading heading="Apartment Search" />
-			<form action="submit" onSubmit={zipCodeOnSubmitHandler}>
+			{/* <form action="submit" onSubmit={zipCodeOnSubmitHandler}>
 				<Input
 					ref={zipCodeRef}
 					placeholder="Zipcode"
 					className="text-black text-md focus:outline-none"
 					maxLength={5}
 				/>
-			</form>
-			<ApartmentsList
-				apartments={queriedApartments}
-				setApartments={setQueriedApartments}
-			/>
+			</form> */}
+
+			<div className="search-container">
+				<PlacesAutoComplete setSelected={setSelected} />
+			</div>
+
+			<ApartmentsList apartments={queriedApartments} setApartments={setQueriedApartments} />
 		</ApartmentSearchContainer>
-	)
+	);
 }
 
-export default ApartmentSearch
+export default ApartmentSearch;
