76146a1c5e736fbc14a5ed651813b3aba3438008
Danny Chen <dannychen8102@gmail.com>
2022-12-02 20:32:43 -0500

    Tyler's code, allows for users to login and accurately display info

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,40 +1,66 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { get_data_first, get_data_last, get_email, get_email_holder } from "../api/getUserProfile";
 
+let holder;
 const UserContext = React.createContext({
-	firstName: '',
-	lastName: '',
-	emailAddress: '',
-	bio: '',
+	firstName: "",
+	lastName: "",
+	emailAddress: "",
+	bio: "",
 	isLoggedIn: false,
 	onLogout: () => {},
 	onLogin: () => {},
-})
+	onFirstNameChange: () => {},
+	onLastNameChange: () => {},
+	onEmailChange: () => {},
+	onRefershHandler: () => {},
+});
 
 export const UserContextProvider = (props) => {
-	const firstName = 'Kan'
-	const lastName = 'Ken'
-	const emailAddress = 'kmaddela@nyit.edu'
+	const [firstName, setFirstName] = useState(localStorage.getItem("firstName"));
+	const [lastName, setLastName] = useState(localStorage.getItem("lastName"));
+	const [emailAddress, setEmail] = useState(localStorage.getItem("emailAddress"));
 	const bio =
-		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolores nam rem enim amet, veniam minima facere nobis magni, tempora, expedita magnam architecto pariatur quas?'
-	const [isLoggedIn, setIsLoggedIn] = useState(
-		localStorage.getItem('isLoggedIn') !== null
-	)
+		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolores nam rem enim amet, veniam minima facere nobis magni, tempora, expedita magnam architecto pariatur quas?";
+	const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("isLoggedIn") !== null);
 
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 
 	const onLogoutHandler = () => {
-		navigate('/welcome')
-		localStorage.removeItem('isLoggedIn')
-		setIsLoggedIn(false)
-	}
+		navigate("/welcome");
+		localStorage.removeItem("isLoggedIn");
+		localStorage.removeItem("firstName");
+		localStorage.removeItem("lastName");
+		localStorage.removeItem("emailAddress");
+		setIsLoggedIn(false);
+	};
 
 	const onLoginHandler = () => {
-		navigate('/apartments')
-		localStorage.setItem('isLoggedIn', '1')
-		setIsLoggedIn(true)
-	}
+		navigate("/apartments");
+		localStorage.setItem("isLoggedIn", "1");
+		setIsLoggedIn(true);
+		onFirstNameChange(get_email_holder());
+		onLastNameChange(get_email_holder());
+		onEmailChange(get_email_holder());
+	};
 
+	const onFirstNameChange = (email) => {
+		get_data_first(setFirstName, email);
+		localStorage.setItem("firstName", holder);
+	};
+
+	const onLastNameChange = (email) => {
+		get_data_last(setLastName, email);
+		localStorage.setItem("lastName", holder);
+	};
+
+	const onEmailChange = (email) => {
+		get_email(setEmail, email);
+		localStorage.setItem("emailAddress", holder);
+	};
+
+	const onRefershHandler = () => {};
 	return (
 		<UserContext.Provider
 			value={{
@@ -45,11 +71,15 @@ export const UserContextProvider = (props) => {
 				isLoggedIn: isLoggedIn,
 				onLogout: onLogoutHandler,
 				onLogin: onLoginHandler,
+				onFirstNameChange: onFirstNameChange,
+				onLastNameChange: onLastNameChange,
+				onEmailChange: onEmailChange,
+				onRefershHandler: onRefershHandler,
 			}}
 		>
 			{props.children}
 		</UserContext.Provider>
-	)
-}
+	);
+};
 
-export default UserContext
+export default UserContext;
