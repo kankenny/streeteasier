b4b5736c8528448717e03e64e0695971c65a0729
Kenny <kenji.maddela01@gmail.com>
2023-01-09 16:38:25 -0500

    Deploy StreetEasier Beta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { UserContextProvider } from './context/UserContext'
 import App from './App'
 import 'tw-elements'
@@ -8,10 +8,10 @@ import 'tw-elements'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<Router>
 			<UserContextProvider>
 				<App />
 			</UserContextProvider>
-		</BrowserRouter>
+		</Router>
 	</React.StrictMode>
 )
